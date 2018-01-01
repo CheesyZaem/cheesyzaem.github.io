9859776d3a75e3c1bf0dc2b4195e50f38e889586
CheesyZaem <yatogamitetsu02@gmail.com>
2018-01-01 14:30:55 +0800

    v1.1
    + commands.html

--- js/main.js
+++ js/main.js
@@ -38,35 +38,6 @@ function main() {
         offset: 80
     });
 
-  	/*====================================
-    Portfolio Isotope Filter
-    ======================================*/
-    $(window).load(function() {
-        var $container = $('.portfolio-items');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-        });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
-
-    });
 
   	/*====================================
     CounterUp
